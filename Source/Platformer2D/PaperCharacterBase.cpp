0fa4896bb73f1c26444347f6d7c70e93bfa0f316
Dominik Kurasbediani <dominik_k@cherrypickgames.com>
2022-03-26 17:04:28 +0100

    Init commit

---
+++ Source/Platformer2D/PaperCharacterBase.cpp
@@ -0,0 +1,245 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PaperCharacterBase.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "PaperFlipbookComponent.h"
+#include "Math/Vector.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/BoxComponent.h"
+#include "DrawDebugHelpers.h"
+
+#define COLLISION_GRAPPABLE		ECC_GameTraceChannel1
+#define DETECTION_GRAPPABLE		ECC_GameTraceChannel2
+
+const FName GrappleSocket = "Grapple Location";
+
+APaperCharacterBase::APaperCharacterBase()
+{
+	BoxCollider = CreateDefaultSubobject<UBoxComponent>(TEXT("Grapple Detection"));
+	BoxCollider->SetupAttachment(GetCapsuleComponent());
+	BoxCollider->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	BoxCollider->SetCollisionObjectType(DETECTION_GRAPPABLE);
+	BoxCollider->SetGenerateOverlapEvents(true);
+	BoxCollider->SetCollisionResponseToAllChannels(ECR_Ignore);
+	BoxCollider->SetCollisionResponseToChannel(COLLISION_GRAPPABLE, ECR_Overlap);
+	BoxCollider->SetBoxExtent(FVector(420.f, 80.f, 280.f));
+	BoxCollider->SetRelativeLocation(FVector(0.f, 0.f, 420.f));
+	m_springArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm"));
+	m_springArm->TargetArmLength = 1200.f;
+	m_springArm->SetupAttachment(GetSprite());
+
+	m_cameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	m_cameraComponent->SetupAttachment(m_springArm);
+
+	m_springArm->AddLocalRotation(FRotator(0, 0, -90));
+	m_springArm->bUsePawnControlRotation = true;
+	m_springArm->bInheritPitch = false;
+	m_springArm->bInheritRoll = false;
+	m_springArm->bInheritYaw = false;
+	
+	// Ground movement
+	GetCharacterMovement()->JumpZVelocity = 900.f;
+	GetCharacterMovement()->MaxAcceleration = 2048;
+	GetCharacterMovement()->bUseSeparateBrakingFriction = true;
+	GetCharacterMovement()->BrakingFriction = 10;
+	GetCharacterMovement()->GroundFriction = 8.0f;
+
+	// Air movement
+	JumpMaxCount = 2;
+	GetCharacterMovement()->AirControl = 1.0f;
+	GetCharacterMovement()->AirControlBoostMultiplier = 2.0f;
+	GetCharacterMovement()->FallingLateralFriction = 10.0f;
+	GetCharacterMovement()->bConstrainToPlane = true;
+	GetCharacterMovement()->bOrientRotationToMovement = false;
+
+	GetCharacterMovement()->SetPlaneConstraintNormal(FVector(0.f, -1.0f, 0.f));
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+	m_pIsDashInCooldown = false;
+	m_pIsDashActivated = false;
+	m_pJumpsRemaining = maxJumps;
+	m_pCanGrapple = false;
+	m_pIsGrappleActivated = false;
+}
+
+void APaperCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+
+	BoxCollider->OnComponentBeginOverlap.AddDynamic(this, &APaperCharacterBase::OnGrappleDetectionOverlapBegin);
+	BoxCollider->OnComponentEndOverlap.AddDynamic(this, &APaperCharacterBase::OnGrappleDetectionOverlapEnd);
+}
+
+void APaperCharacterBase::Tick(float deltaTime)
+{
+	Super::Tick(deltaTime);
+
+	if (!GetCharacterMovement()->IsFalling())
+	{
+		m_pJumpsRemaining = maxJumps;
+	}
+	// ANIMATIONS //////////////////////////////////
+	if (!IsMovementBlocked())
+	{
+		if (GetCharacterMovement()->GetLastUpdateVelocity().Length() == 0.0f)
+		{
+			GetSprite()->SetFlipbook(m_IdleAnimation);
+		}
+		else
+		{
+			if (GetCharacterMovement()->GetLastUpdateVelocity().X > 0)
+				GetSprite()->SetWorldRotation(FRotator(0, 0, 0));
+			else if (GetCharacterMovement()->GetLastUpdateVelocity().X < 0)
+				GetSprite()->SetWorldRotation(FRotator(0, -180, 0));
+			if (m_RunningAnimation && GetCharacterMovement()->GetLastUpdateVelocity().Z == 0)
+				GetSprite()->SetFlipbook(m_RunningAnimation);
+		}
+		if (GetCharacterMovement()->IsFalling())
+		{
+			if (m_FallingAnimation && m_JumpingAnimation)
+			{
+				if (GetCharacterMovement()->GetLastUpdateVelocity().Z > 0 && m_JumpingAnimation)
+					GetSprite()->SetFlipbook(m_JumpingAnimation);
+				else if (GetCharacterMovement()->GetLastUpdateVelocity().Z < 0 && m_FallingAnimation)
+					GetSprite()->SetFlipbook(m_FallingAnimation);
+			}
+			else if (!m_FallingAnimation && GetCharacterMovement()->GetLastUpdateVelocity().Z != 0)
+			{
+				GetSprite()->SetFlipbook(m_JumpingAnimation);
+			}
+
+		}
+	}
+	//////////////////////////////////////////////
+
+	// Dash Handling ////////////////////////////////////////////////////////////
+	if (m_pIsDashActivated)
+	{
+		m_pIsDashInCooldown = true;
+		if (m_pDashTimeElapsed < dashDuration)
+		{
+			LaunchCharacter(GetDashVelocity(), true, true);
+			m_pDashTimeElapsed += deltaTime;
+		}
+		else
+		{
+			m_pIsDashActivated = false;
+			GetWorldTimerManager().SetTimer(m_pDashTimerHandle, this, &APaperCharacterBase::OnDashCooldownTimerOver, timerCooldown, false);
+		}
+	}
+	/////////////////////////////////////////////////////////////////////////
+
+	if (m_pIsGrappleActivated)
+	{
+		DrawDebugLine(GetWorld(), GetActorLocation(), m_pGrappableLocation, FColor::Red, false, -1.f, 0U, 5.0f);
+	}
+}
+
+
+void APaperCharacterBase::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &APaperCharacterBase::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &APaperCharacter::StopJumping);
+	PlayerInputComponent->BindAction("Dash", IE_Pressed, this, &APaperCharacterBase::Dash);
+	PlayerInputComponent->BindAction("Grapple", IE_Pressed, this, &APaperCharacterBase::Grapple);
+	PlayerInputComponent->BindAxis("MoveRight", this, &APaperCharacterBase::MoveRight);
+}
+
+void APaperCharacterBase::OnGrappleDetectionOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherComp->GetCollisionObjectType() == COLLISION_GRAPPABLE)
+	{
+		m_pCanGrapple = true;
+		m_pGrappableLocation = OtherComp->GetSocketLocation("Grapple Location");
+		UE_LOG(LogTemp, Warning, TEXT("Grapple Location: %s"), *m_pGrappableLocation.ToString());
+	}
+}
+
+void APaperCharacterBase::OnGrappleDetectionOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if (OtherComp->GetCollisionObjectType() == COLLISION_GRAPPABLE)
+	{
+		m_pCanGrapple = false;
+		//m_pGrappableLocation = OtherComp->GetSocketLocation("Grapple Location");
+		m_pIsGrappleActivated = false;
+		UE_LOG(LogTemp, Warning, TEXT("Grapple Detection Overlap End"));
+	}
+}
+
+void APaperCharacterBase::MoveRight(float value)
+{
+	AddMovementInput(FVector(1.0, 0, 0), value);
+}
+
+void APaperCharacterBase::Dash()
+{
+	if (!m_pIsDashInCooldown && GetVelocity().X != 0.f)
+	{
+		if (m_DashAnimation)
+			GetSprite()->SetFlipbook(m_DashAnimation);
+		m_pDashTimeElapsed = 0.f;
+		m_pIsDashActivated = true;
+		m_pDashTargetLocation = GetActorLocation() + GetSprite()->GetForwardVector() * dashDistance;
+		m_pDashStartLocation = GetActorLocation();
+	}
+}
+
+void APaperCharacterBase::Grapple()
+{
+	if (m_pCanGrapple)
+	{
+		FVector direction = m_pGrappableLocation - GetActorLocation();
+		m_pIsGrappleActivated = true;
+		direction.Y = 0.f;
+		//DrawDebugLine(GetWorld(), GetActorLocation(), GetActorLocation() + direction*grappleStrengthCoefficient, FColor::Red, true, 5.0f);
+		LaunchCharacter(direction * grappleStrengthCoefficient, true, true);
+	}
+}
+
+void APaperCharacterBase::Jump()
+{
+	FHitResult hit;
+	if (DetectWall(hit) && GetCharacterMovement()->IsFalling())
+	{
+		WallJump(hit);
+		return;
+	}
+	if (m_pJumpsRemaining > 0)
+	{
+		APaperCharacter::Jump();
+		m_pJumpsRemaining--;
+	}
+}
+
+void APaperCharacterBase::WallJump(FHitResult& hit)
+{
+	float sign = FMath::Sign(GetActorLocation().X - hit.Location.X);
+	FVector launchVelocity = FVector(sign * wallJumpHorizontalStrength, 0.0f, GetCharacterMovement()->JumpZVelocity);
+	LaunchCharacter(launchVelocity, true, true);
+}
+
+bool APaperCharacterBase::DetectWall(FHitResult& OutHit)
+{
+	FCollisionQueryParams params;
+	params.AddIgnoredActor(this->GetOwner()); 
+	float radius = GetCapsuleComponent()->GetScaledCapsuleRadius() + raycastDistance;
+	//FVector startPos = GetActorLocation() - GetSprite()->GetForwardVector() * radius;
+	FVector startPos = GetActorLocation();
+	FVector endPos = GetActorLocation() + GetSprite()->GetForwardVector() * radius;
+	DrawDebugLine(GetWorld(), startPos, endPos, FColor::Red);
+	return GetWorld()->LineTraceSingleByChannel(OutHit, startPos, endPos, ECC_Visibility, params);
+
+}
+
+void APaperCharacterBase::OnDashCooldownTimerOver()
+{
+	m_pIsDashInCooldown = false;
+	GetWorldTimerManager().ClearTimer(m_pDashTimerHandle);
+}
+
+
+
---
+++ Source/Platformer2D/PaperCharacterBase.h
@@ -0,0 +1,99 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperCharacter.h"
+#include "PaperCharacterBase.generated.h"
+
+class UPaperFlipbook;
+
+/**
+ * 
+ */
+UCLASS()
+class PLATFORMER2D_API APaperCharacterBase : public APaperCharacter
+{
+	GENERATED_BODY()
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+	class USpringArmComponent* m_springArm;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+	class UCameraComponent* m_cameraComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Animations)
+	UPaperFlipbook* m_IdleAnimation;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Animations)
+	UPaperFlipbook* m_RunningAnimation;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Animations)
+	UPaperFlipbook* m_FallingAnimation;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Animations)
+	UPaperFlipbook* m_JumpingAnimation;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Animations)
+	UPaperFlipbook* m_DashAnimation;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = MovementMechanics)
+	int maxJumps = 2;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = MovementMechanics)
+	float dashDistance = 1000.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = MovementMechanics)
+	float dashDuration = 0.5f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = MovementMechanics)
+	float timerCooldown = 0.5f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = MovementMechanics)
+	float wallJumpHorizontalStrength = 4500.f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = MovementMechanics)
+	float grappleStrengthCoefficient = 2.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=ObstacleDetection)
+	float raycastDistance = 15.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=CollisionDetection)
+	class UBoxComponent* BoxCollider;
+
+	bool m_pIsDashInCooldown;
+	FTimerHandle m_pDashTimerHandle;
+	bool m_pIsDashActivated;
+	float m_pDashTimeElapsed;
+	FVector m_pDashTargetLocation;
+	FVector m_pDashStartLocation;
+	int m_pJumpsRemaining;
+
+	FVector m_pGrappableLocation;
+	bool m_pCanGrapple;
+	bool m_pIsGrappleActivated;
+
+public:
+	APaperCharacterBase();
+
+	FORCEINLINE bool IsMovementBlocked() const { return m_pIsDashActivated; }
+	FORCEINLINE FVector GetDashVelocity() const { return (m_pDashTargetLocation - m_pDashStartLocation) / dashDuration;}
+
+protected:
+		void MoveRight(float value);
+		void Dash();
+		void Jump();
+		void WallJump(FHitResult& hit);
+		void Grapple();
+		UFUNCTION()
+		void OnDashCooldownTimerOver();
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION()
+	void OnGrappleDetectionOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnGrappleDetectionOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	virtual void Tick(float deltaTime) override;
+	bool DetectWall(FHitResult& OutHit1);
+
+
+private:
+	
+};
